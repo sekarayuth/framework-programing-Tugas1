a9d1ff695d2cccf6ab9d7889781100d4636e38c0
sekarayuth <sekarayuth@gmail.com>
2025-11-12 21:24:12 +0700

    Delete kelurahan/warga directory

--- kelurahan/warga/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import WargaListView, WargaDetailView, PengaduanListView
-
-
-urlpatterns = [
-    path('', WargaListView.as_view(), name='warga-list'),
-    path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
-    path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
-]
