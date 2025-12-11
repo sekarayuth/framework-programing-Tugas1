30d84baf78489c47a9214a4ff196c7c4f95e3fc3
sekarayuth <sekarayuth@gmail.com>
2025-12-11 10:52:20 +0700

    Add files via upload

---
+++ sekar/kelurahan/warga/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from .models import Warga, Pengaduan
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .forms import PengaduanForm
+
+from .models import Warga, Pengaduan
+from .forms import WargaForm 
+
+from rest_framework import viewsets
+from .serializers import WargaSerializer, PengaduanSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAdminUser
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+class WargaListView(ListView):
+    model = Warga
+
+class WargaDetailView(DetailView):
+    model = Warga
+
+class PengaduanListView(ListView):
+    model = Pengaduan
+
+class WargaCreateView(CreateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga-list')
+
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('daftar_pengaduan')
+
+class WargaUpdateView(UpdateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html' # Kita pakai template yang sama
+    success_url = reverse_lazy('warga-list')
+
+class WargaDeleteView(DeleteView):
+    model = Warga
+    template_name = 'warga/warga_confirm_delete.html'
+    success_url = reverse_lazy('warga-list')
+
+class PengaduanUpdateView(UpdateView):
+    model = Pengaduan
+    fields = ['judul', 'isi', 'tanggal', 'status']
+    template_name = 'pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list')
+
+
+class PengaduanDeleteView(DeleteView):
+    model = Pengaduan
+    template_name = 'pengaduan_confirm_delete.html'
+    success_url = reverse_lazy('pengaduan-list')
+
+class WargaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Warga.objects.all().order_by('-tanggal_registrasi')
+    serializer_class = WargaSerializer
+    permission_classes = [IsAdminUser]
+
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['nama_lengkap', 'nik', 'alamat']
+    ordering_fields = ['nama_lengkap', 'tanggal_registrasi']
+
+class PengaduanViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows pengaduan to be viewed or edited.
+    """
+    queryset = Pengaduan.objects.all().order_by('-tanggal_lapor')
+    serializer_class = PengaduanSerializer
+
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['judul', 'deskripsi']
+    ordering_fields = ['status', 'tanggal_lapor']
